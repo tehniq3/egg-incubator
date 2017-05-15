cbc6921f8c65858731bd12a5f623832c028845b1
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2017-05-15 11:15:18 +0300

    Update eggincubator_1d.ino

--- eggincubator_1d.ino
+++ eggincubator_1d.ino
@@ -71,8 +71,8 @@ DallasTemperature sensors(&oneWire);
 // define pins for relay or leds
 #define temppeste A1
 #define umidpeste A2
-#define motoras 5
-#define eroare 4
+#define motoras 9
+#define eroare 13
 
 // define variable
 float te, t1, tes, dete, ete;   // variable for temperature
